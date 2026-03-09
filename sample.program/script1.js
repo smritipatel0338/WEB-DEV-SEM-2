c1c3a79565f88333480f103003eef9b30e4c3942
smritipatel0338 <www.smritipatel123@gmail.com>
2026-03-09 18:25:50 +0530

    learning loops concepts

---
+++ sample.program/script1.js
@@ -0,0 +1,62 @@
+  /* let day=3;
+        switch(day){
+            case 1:
+                console.log("monday");
+                break;
+            case 2:
+                console.log("tuesday");
+                break;
+            case 3:
+                console.log("wednesday");
+                break;
+            case 4:
+                console.log("thursday");
+                break;
+            case 5:
+                console.log("friday");
+                break;
+            case 6:
+                console.log("saturday");
+                break;
+            case 7:
+                console.log("sunday");
+                break;
+            default:
+                console.log("invalid day");
+        }
+ 
+        let marks=5;
+        if  (marks>=90){
+            console.log("grade A");
+        }
+        else if (marks>=80){
+            console.log("grade B");
+        }
+        else if (marks>=70){
+            console.log("grade C");
+        }
+        else if (marks>=60){
+            console.log("grade D");
+        }
+        else{
+            console.log("grade F");
+        }
+
+        for (let i=1; i<=5; i++){
+            console.log(i);
+        }
+            
+
+        let i=1;
+        while (i<=5){
+            console.log(i);
+            i++;
+        }
+        */
+        let i=1;
+        do {
+            console.log(i);
+            i++;
+        } while (i<=5);
+
+            
